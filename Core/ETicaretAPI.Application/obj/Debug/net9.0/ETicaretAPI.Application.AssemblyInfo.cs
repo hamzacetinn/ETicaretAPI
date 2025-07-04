68218702c912ac27d5e4c8e6943e1c9ae5a8e751
Hamza Çetin <hamzacetinn24@gmail.com>
2025-07-04 13:11:18 +0300

    sil

--- Core/ETicaretAPI.Application/obj/Debug/net9.0/ETicaretAPI.Application.AssemblyInfo.cs
+++ Core/ETicaretAPI.Application/obj/Debug/net9.0/ETicaretAPI.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ETicaretAPI.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3958b9f50483c6ee87d3554ebf6e50d7841e6412")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8611270cdc7ef22df48caf415a72fd114a5e721")]
 [assembly: System.Reflection.AssemblyProductAttribute("ETicaretAPI.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ETicaretAPI.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
